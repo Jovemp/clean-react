6e52f1cc62b2358aeaaedcf7594d9571edda9b0d
Paulo Edson <jovemp@hotmail.com>
2020-11-08 16:08:05 -0300

    feat: add app router

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Styles from './form-status-styles.scss';
+import Styles from './form-status-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
 
 const FormStatus: React.FC = () => {
--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -2,7 +2,6 @@ import React, { memo } from 'react'
 import Logo from '@/presentation/components/logo/logo'
 import Styles from './login-header-styles.scss'
 
-
 const LoginHeader: React.FC = () => {
   return (
     <header className={Styles.header}>
