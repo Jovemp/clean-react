bec1e543a35c845c0e4effcf114a7ff8fbcb87f0
Paulo Edson <jovemp@hotmail.com>
2020-11-14 16:25:36 -0300

    feat: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/errors/invalid-field-error.ts
@@ -0,0 +1,6 @@
+export default class InvalidFieldError extends Error {
+  constructor (fieldLabel: string) {
+    super(`O campo ${fieldLabel} é inválido`)
+    this.name = 'InvalidFieldError'
+  }
+}
